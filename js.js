fb959b0973aa32ec8d6dac97917d10fe66070612
woutcorijn <wout.corijn@hotmail.com>
2019-03-31 20:34:44 +0200

    V0.21

--- js.js
+++ js.js
@@ -3,11 +3,11 @@ var CardType = "C";
 var RandNumber = ["1","2","3","4","5","6","7","8","9","10","J","Q","K"];
 var RandCardType = ["C","D","H","S"];
 
-function Card_function() {
-	Number = document.getElementById("Number_select").value;
-	CardType = document.getElementById("Type_select").value;
+function Main_function() {
+		console.log("Main");
 	document.getElementsByClassName("number")[0].innerHTML = Number;
 	document.getElementsByClassName("number")[1].innerHTML = Number;
+	
 	if (CardType == "D" || CardType == "H") {
 		document.getElementsByClassName("number")[0].style.color = "red";
 		document.getElementsByClassName("number")[1].style.color = "red";
@@ -15,21 +15,38 @@ function Card_function() {
 		document.getElementsByClassName("number")[0].style.color = "Black";
 		document.getElementsByClassName("number")[1].style.color = "Black";
 	}
-	document.getElementById("drawing").src = "images/" + Number + CardType + ".png"
+	
+}
+
+function Card_function() {	
+console.log("card");
+	Number = document.getElementById("Number_select").value;
+	CardType = document.getElementById("Type_select").value;
+	Main_function()
+    document.getElementById("drawing").src = "images/" + Number + CardType + ".png"	
 }
 
 function random_button() {
+console.log("Random");
 	Number = RandNumber[Math.floor(Math.random() * 12)];
-	CardType = RandCardType[Math.floor(Math.random() * 3)];	
-	document.getElementsByClassName("number")[0].innerHTML = Number;
-	document.getElementsByClassName("number")[1].innerHTML = Number;
-		if (CardType == "D" || CardType == "H") {
-		document.getElementsByClassName("number")[0].style.color = "red";
-		document.getElementsByClassName("number")[1].style.color = "red";
-	} else {
-		document.getElementsByClassName("number")[0].style.color = "Black";
-		document.getElementsByClassName("number")[1].style.color = "Black";
-	}
+	CardType = RandCardType[Math.floor(Math.random() * 3)];
+	document.getElementById("Number_select").value = Number;
+	document.getElementById("Type_select").value= CardType;
+	Main_function()
 	document.getElementById("drawing").src = "images/" + Number + CardType + ".png"
+}
+
+function turn_button() {
+	if (document.getElementById("back").style.display == "block") {
+	document.getElementById("back").style.display = "none";
+	document.getElementById("drawing").style.display = "block";
+	document.getElementsByClassName("number")[0].style.display = "block";
+	document.getElementsByClassName("number")[1].style.display = "block";
+	} else {
+	document.getElementById("back").style.display = "block"
+	document.getElementById("drawing").style.display = "none";
+	document.getElementsByClassName("number")[0].style.display = "none";
+	document.getElementsByClassName("number")[1].style.display = "none";
+	}
 }
 
